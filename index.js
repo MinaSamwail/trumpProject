0b2b9dcb47d088423103c24ebcf766e688f6ca3f
Mina Samwail <minasamwail@MacBook-Air-de-Mina.local>
2020-11-24 11:29:52 +0100

    creation de la fonction startGame et resetGame

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ let ballPositionX = canvas.width / 2;
 let ballSpeedX = 10;
 let ballSpeedY = 5;
 // let ballSize = 20;
+
+//  mesure de la réactivité des sites web
 let framePerSecond = 60;
 
 //parametre de la raquete
@@ -21,27 +23,56 @@ let playerOneScore = 0;
 let playerTwoScore = 0;
 
 let startGame = document.getElementById("startGame");
-let pauseTheGame = document.getElementById("pauseTheGame");
+// let pauseTheGame = document.getElementById("pauseTheGame");
 let gameOver = document.getElementById("gameOver");
-let container = document.getElementById("container");
+let gameplay = document.getElementById("gameplay");
 
 let startBtn = document.getElementsByClassName("startBtn");
-let continueBtn = document.getElementsByClassName("continueBtn");
-let restartBtn = document.getElementsByClassName("restartBtn");
+// let continueBtn = document.getElementsByClassName("continueBtn");
+// let restartBtn = document.getElementsByClassName("restartBtn");
 let tryAgainBtn = document.getElementsByClassName("tryAgainBtn");
 let theField = document.getElementsByClassName("theField");
 let gameMessage = document.getElementsByClassName("gameMessage");
 
 //Parametre
 let scoreToWin = 10;
-let gamePaused = false;
+// let gamePaused = false;
 let gameInProgress = false;
 
 //Afin d'avoir la largeur et la hauteur de la fenetre
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+//placement des raquettes
+raquette1Y = canvas.height / 2 - raquetteHeight / 2;
+raquette2Y = canvas.height / 2 - raquetteHeight / 2;
 
-function drawTheGame() {
-  //   canvasContext.fillRect = "black";
-  canvasContext.clearRect(0, 0, canvas.height, canvas.width);
+//liste des className active or not
+startMenu.className = "active";
+gameplay.className = "";
+gameOver.className = "";
+
+startBtn.addEventListener("click", startGame);
+tryAgainBtn.addEventListener("click", resetGame);
+document.addEventListener("keydown", goDown);
+
+function startGame() {
+  gameInProgress = true;
+  gameplay.className = "";
+  startMenu.className = "";
+  gameOver.className = "";
+  window.setInterval(function () {
+    moveEverything();
+    plateform();
+  }, 1000 / fps);
 }
+
+function resetGame() {
+  playerOneScore = 0;
+  playerTwoScore = 0;
+  startGame();
+}
+
+// function drawTheGame() {
+// canvasContext.fillRect = "black";
+//   canvasContext.clearRect(0, 0, canvas.height, canvas.width);
+// }
